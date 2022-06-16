3296fc2204f20e0245c63393ddb776fc9d862b89
Michael Perry <moperrry@gmail.com>
2022-06-16 11:32:59 -0400

    functions working

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,4 @@
+import string
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -8,24 +9,44 @@ def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file. my code does here"""
     print(f'Your file is: {file}')
     with open(file) as open_file:
-        read_file= open_file.readlines()
-    print(read_file)
+        read_file= open_file.read()
+        remove_punct = remove_punctuation(read_file)
+        lower_case = use_lowercase(remove_punct)
+        remove_stop = remove_stop_words(lower_case)
+        frequency_count = frequency_counter(remove_stop)
+    print(frequency_count)
         #print(len(open_file))
-        
-#step 2: calculate the frequency of words in the file
-freqs = {}
-for word in text:
-    if word not in freqs:
-        freqs[word] = 1
-    else:
-        freqs[word] += 1
-file.close()
+# def split_text(text):
+#     return text.split()
 
-#step 3: Create funciton possibly (reg x) to remove punctiation
+#step 2: calculate the frequency of words in the file
+def frequency_counter(text):
+    freqs = {}
+    for word in text:
+        if word in freqs:
+            freqs[word] += 1
+        else:
+            freqs[word] = 1
+    return freqs
+
+#step 3: Create funciton possibly (reg x) to remove punctuation
     #text.translate(str.maketrans('', '', string.punctuation))
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
 #step 4: use lowercase function
+def use_lowercase(text):
+    return text.lower()
 #step 5: remove "stop words"
+def remove_stop_words(text):
+    text_list = text.split()
+
+    for word in text_list.copy():
+        if word in STOP_WORDS:
+            text_list.remove(word)
+    return text_list
+
 #step 6: posisbly use dictionary to count each word
+
 #step 7: find a way to format print statement...f string
 
 
